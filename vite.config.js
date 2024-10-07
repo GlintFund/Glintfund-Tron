14b8ff2434b5175b5ccde760411386c1eb7f8a66
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-10-07 12:54:26 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import environment from "vite-plugin-environment";
-import dotenv from "dotenv";
-
-dotenv.config({ path: "./src/.env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), environment("all", { prefix: "REACT_APP" })],
+  plugins: [react(), environment("all", { prefix: "VITE_" })],
   server: {
     open: true, // automatically open in the browser
   },
