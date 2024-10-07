00523a0f710a1855bf1376c2f7affc9772aee63c
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-10-07 18:53:15 +0100

    [feat] - added lending and borrowing modal

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import environment from "vite-plugin-environment";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config({ path: "./src/.env" });
+// dotenv.config({ path: "./src/.env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // environment("all", { prefix: "VITE_" }),
-    environment("all", { prefix: "REACT_APP_" }),
+    environment("all", { prefix: "VITE_" }),
+    // environment("all", { prefix: "REACT_APP_" }),
   ],
 });
