28e504345b2cfcff4d64fdecb2ac9282fc932bfc
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-10-08 23:40:00 +0100

    used react router link

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import environment from "vite-plugin-environment";
+import environment from "vite-plugin-environment";
 // import dotenv from "dotenv";
 
-dotenv.config({ path: "./src/.env" });
+// dotenv.config({ path: "./src/.env" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
